0fff3993f57e8473559673612f820024ba81cdd8
Andrzej Perczak <linux@andrzejperczak.com>
2022-06-15 11:34:36 +0200

    config: Optimize Launcher3 and Settings for speed
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- config/common.mk
+++ config/common.mk
@@ -256,6 +256,11 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     CarSystemUI \
     SystemUI
 
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep \
+    Settings \
+    TrebuchetQuickStep
+
 PRODUCT_PRODUCT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
 
