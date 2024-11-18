b4e6e299430dd2a321ec166266a95c128739c560
Woody Lin <woodylin@google.com>
2024-11-18 11:39:30 +0800

    config: Enable zygote critical window
    Bug: 141948707
    Change-Id: I5dc57312118b0f6d95fc9fe164363ec7e15f1b51

--- config/common.mk
+++ config/common.mk
@@ -317,3 +317,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Shutdown
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.build.shutdown_timeout?=0
+
+# Zygote
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    zygote.critical_window.minute?=10
