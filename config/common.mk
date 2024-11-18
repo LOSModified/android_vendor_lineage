cd6a4120934287a2dff0c7976a83076709e218ee
someone5678 <someone5678@users.noreply.github.com>
2024-11-18 11:36:08 +0800

    config: Diasble shutdown timeout by default
    Change-Id: If310d56f20982e3108300c66073c57a47ae6c74d

--- config/common.mk
+++ config/common.mk
@@ -313,3 +313,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_VENDOR_PROPERTIES += \
     ro.egl.blobcache.multifile=true \
     ro.egl.blobcache.multifile_limit=33554432
+
+# Shutdown
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.build.shutdown_timeout?=0
